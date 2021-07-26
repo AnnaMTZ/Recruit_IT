30b888133b6bea4ede567038b4c66b4602f19377
Woest <redmar.woest@capgemini.com>
2021-07-26 13:45:42 +0200

    adding login

---
+++ to-do.js
@@ -0,0 +1,10 @@
+// JS all required elements
+const inputBox = document.querySelector(".to-do-inputfield input")
+const addButton = document.querySelector(".to-do-inputfield button")
+const removeButton = document.querySelector(".to-do-list button")
+
+
+
+// Add tasks to button
+
+// Make everything
